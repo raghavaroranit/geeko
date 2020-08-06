4c0f1e779bd855fa55bcabcd97d49907668b6473
Raghav Arora <68223093+raghavaroranit@users.noreply.github.com>
2020-08-06 16:07:44 +0530

    Create webhooks.py

---
+++ webhooks.py
@@ -0,0 +1,20 @@
+def main():
+    url = '<webhook URL>' ## Provude the webhook URL
+    bot_message = {
+        'text' : 'XYZ'} ## Provide the message
+
+    message_headers = { 'Content-Type': 'application/json; charset=UTF-8'}
+
+    http_obj = Http()
+
+    response = http_obj.request(
+        uri=url,
+        method='POST',
+        headers=message_headers,
+        body=dumps(bot_message),
+    )
+
+    print(response)
+
+if __name__ == '__main__':
+    main()
